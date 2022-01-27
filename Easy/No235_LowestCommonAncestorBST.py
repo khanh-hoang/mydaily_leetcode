b0da1321f14968dafeddc56c923a432f7f2bebe2
Your Name <you@example.com>
2022-01-27 20:52:57 +0200

    create no235

---
+++ Easy/No235_LowestCommonAncestorBST.py
@@ -0,0 +1,31 @@
+#Time: O(n)
+#Space:O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        node = root
+        while node:
+            if p.val > node.val and q.val > node.val:
+                node = node.right
+            elif p.val < node.val and q.val < node.val:
+                node = node.left
+            else:
+                return node
+        
+        """
+        Time: O(n)
+        Space:O(n)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root 
+        """
+        

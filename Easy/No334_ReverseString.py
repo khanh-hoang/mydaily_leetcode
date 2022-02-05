c47f48beb2c054d74152aa9e54f3c85a792df2cb
Your Name <you@example.com>
2022-02-05 22:01:42 +0200

    create no334

---
+++ Easy/No334_ReverseString.py
@@ -0,0 +1,13 @@
+#Time: O(n)
+#Space:O(1)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s) - 1
+        for i in range(len(s)//2):
+            s[i], s[n-i] = s[n-i], s[i]
+        """
+        s.reverse()
+        """

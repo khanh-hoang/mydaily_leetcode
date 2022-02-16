50615d2d9114d79f4d6e8501f5cb3e0860da03b1
Your Name <you@example.com>
2022-02-16 23:10:20 +0200

    create no461

---
+++ Easy/No461_HammingDistance.py
@@ -0,0 +1,10 @@
+#Time: O(1)
+#Space:O(1)
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x^y
+        distance = 0
+        while xor:
+            distance += 1
+            xor = xor & (xor-1)
+        return distance

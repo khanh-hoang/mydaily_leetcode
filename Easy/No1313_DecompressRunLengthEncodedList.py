a38f79b5c4522c8e4e0c071bfee9bdb84b85455e
Your Name <you@example.com>
2022-05-07 23:55:03 +0300

    create no1313

---
+++ Easy/No1313_DecompressRunLengthEncodedList.py
@@ -0,0 +1,8 @@
+#Time: O(n) n: output length
+#Space:O(n) 
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(0, len(nums), 2):
+            res += [nums[i+1]] * nums[i]
+        return res

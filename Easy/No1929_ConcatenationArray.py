c375fecc6f8ef123e6ee936c5d186efe9dc2ee44
Your Name <you@example.com>
2022-06-02 23:39:58 +0300

    create no1929

---
+++ Easy/No1929_ConcatenationArray.py
@@ -0,0 +1,10 @@
+#Time: O(n)
+#Space:O()
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * 2 * n
+        for i in range(n):
+            result[i] = nums[i]
+            result[i+n] = nums[i]
+        return result

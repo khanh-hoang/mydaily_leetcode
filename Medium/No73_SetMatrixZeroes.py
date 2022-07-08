7da57c3ad48a9ea4a8499a142b5147838e8cdff2
Your Name <you@example.com>
2022-07-08 23:39:30 +0300

    create no73

---
+++ Medium/No73_SetMatrixZeroes.py
@@ -0,0 +1,27 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rowzero = False
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        rowzero = True
+                    
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        
+        if rowzero:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0

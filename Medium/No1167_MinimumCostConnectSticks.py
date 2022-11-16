1c9176e994aee4e9e26e0128680b3b5fc18ebd34
Your Name <you@example.com>
2022-11-16 23:57:51 +0100

    create no1167

---
+++ Medium/No1167_MinimumCostConnectSticks.py
@@ -0,0 +1,11 @@
+#Time: O(nlogn)
+#Space:O(n)
+class Solution:
+    def connectSticks(self, sticks: List[int]) -> int:
+        res = 0
+        heapq.heapify(sticks)
+        while len(sticks) > 1:
+            cost = heapq.heappop(sticks) + heapq.heappop(sticks)
+            heapq.heappush(sticks, cost)
+            res += cost
+        return res

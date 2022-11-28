4ca27d724a929236bc8d72b6c5ed17681b47039e
Your Name <you@example.com>
2022-11-28 23:44:35 +0100

    create no1277

---
+++ Medium/No1277_CountSquareSubmatricesWithAllOnes.py
@@ -0,0 +1,9 @@
+#Time: O(n*m)
+#Space:O(1)
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j]:
+                    matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j], matrix[i][j-1]) + 1
+        return sum(map(sum, matrix))
